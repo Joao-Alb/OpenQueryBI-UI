c5806d04da9f3b632f8062b61d6c017f976f7b66
Joao-Alb <joao.lima.alb@gmail.com>
2025-08-23 19:42:14 -0300

    fix: using api to config mcp environment

--- client/benchmark/db_config.py
+++ client/benchmark/db_config.py
@@ -1,17 +1,23 @@
 import json
 import os
+import requests
 
-mcp_databases_path = 'OpenQueryBI\\servers\\OpenQueryBI\\databases.json'
-spider_databases_path = 'data\\spider\\spider_data\\test_tables.json'
+spider_databases_path = 'benchmark/data/spider/spider_data/test_tables.json'
+spider_testcases_path = 'benchmark/data/spider/spider_data/test.json'
 
 def load_file(databases_path):
     with open(databases_path, 'r') as file:
         databases = json.load(file)
     return databases
 
-def save_mcp_databases(databases, databases_path=mcp_databases_path):
-    with open(databases_path, 'w') as file:
-        json.dump(databases, file, indent=4)
+def save_mcp_databases(databases:list):
+    body = databases
+    r = requests.post("http://openquerybi:8001/set_databases_configs/", json=body)
+    #export result of requests to a file for debugging
+    with open("benchmark/db_config_log.txt", "w") as f:
+        f.write(f"Request body 4: {json.dumps(body, indent=2)}\n")
+        f.write(f"Response status code: {r.status_code}\n")
+        f.write(f"Response body: {r.text}\n")
 
 def set_databases(databases_ids:list[str]):
     spider = load_file(spider_databases_path)
@@ -27,9 +33,9 @@ def set_databases(databases_ids:list[str]):
                         "password":"",
                         "host":"",
                         "port":"",
-                        "database":os.path.abspath(f"data\\spider\\spider_data\\test_database\\{id}\\{id}.sqlite")
+                        "database":os.path.abspath(f"test/test_database/{id}/{id}.sqlite")
                     },
                     "tables":db['table_names'],
                     "type": "sqlite"
                 })
-    save_mcp_databases(mcp)
+    save_mcp_databases(mcp["databases"])
