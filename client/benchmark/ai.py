e64490adad35565f1409e06795d217dc04c46620
Joao-Alb <joao.lima.alb@gmail.com>
2025-08-28 23:01:42 -0300

    fix: get sql to spider

--- client/benchmark/ai.py
+++ client/benchmark/ai.py
@@ -1,23 +1,20 @@
-from services.ai_service import get_response
+from utils.async_helpers import run_async
+import streamlit as st
+from services.mcp_service import run_agent
+from utils.ai_prompts import make_main_prompt
+from langchain_core.messages import ToolMessage
 
 def get_sql_from_ai(prompt:str)->str:
-    ans = get_response(prompt, llm_provider='Anthropic')
-    return ans
+    prompt = make_main_prompt(prompt,language='spider-benchmark')
+    ans = run_async(run_agent(st.session_state.agent, prompt))
+    return find_sql_in_text(ans)
 
-def find_sql_in_text(text:str)->str:
-    import re
-    # Regular expression to find SQL statements
-    sql_pattern = re.compile(
-        r"(SELECT\s+.*?;|INSERT\s+INTO\s+.*?;|UPDATE\s+.*?;|DELETE\s+FROM\s+.*?;|CREATE\s+TABLE\s+.*?;|DROP\s+TABLE\s+.*?;|ALTER\s+TABLE\s+.*?;)",
-        re.IGNORECASE | re.DOTALL
-    )
-    
-    matches = sql_pattern.findall(text)
-    if matches:
-        # Return the first matched SQL statement, cleaned up
-        return clean_sql(matches[-1])
-    else:
-        return ""
+def find_sql_in_text(response)->str:
+    for msg in response["messages"][::-1]:
+        if hasattr(msg, 'tool_calls') and msg.tool_calls:
+            for tool_call in msg.tool_calls[::-1]:
+                if "validate_query" in tool_call['name']:
+                    return clean_sql(str(tool_call['args']['query']))
 
 def clean_sql(sql:str)->str:
     return sql.strip().rstrip(';')
